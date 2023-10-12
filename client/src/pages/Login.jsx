fc0cf10b89e23af084899e74010c3ead3d70f627
focal <zoinexion@gmail.com>
2023-10-12 11:23:02 +0700

    Add auth to frontend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,63 +3,90 @@ import { Link } from 'react-router-dom'
 
 export const Login = () => {
 
-  return (
-      <div className="p-32 mainBgColor">
-        <div className="w-full h-screen flex items-start">
-          <div className="relative w-1/2 h-full flex flex-col rounded-xl primaryBgColor">
-            <div className="absolute top-[14%] left-[13%]">
-              <h1 className="text-white font-semibold text-5xl">
-                DON’T FORGET TO <br />
-                COMPLETE YOUR <br />
-                PROFILE
-              </h1>
-              <div className="flex pt-10 ">
-                <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
-                  <p className="text-white font-semibold text-3xl">1</p>
-                </div>
-                <p className="pl-5 text-white font-light text-3xl">
-                  Use that email address and <br />
-                  valid for account verification
-                </p>
-              </div>
-              <div className="flex pt-6 ">
-                <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
-                  <p className="text-white font-semibold text-3xl">2</p>
-                </div>
-                <p className="pt-3 pl-5 text-white font-light text-3xl">
-                  Complete the registration form
-                </p>
-              </div>
-              <div className="flex pt-6 ">
-                <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
-                  <p className="text-white font-semibold text-3xl">3</p>
-                </div>
-                <p className="pl-5 text-white font-light text-3xl">
-                  Congratulations on becoming a <br />
-                  DosenKu member
-                </p>
-              </div>
-              <button className="w-full text-white py-3 px-4 mt-12 rounded-md ring-1 ring-black secondaryBgColor">
-                <Link to={'/signup'}>Make new account</Link>
-              </button>
-            </div>
-          </div>
+  const [email,    setEmail]    = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-          <div className="w-1/2 h-full flex flex-col p-20 pt-60 bg-white rounded-xl">
-            <h1 className="font-semibold text-5xl place-self-center pb-10">Login</h1>
-            <form className="w-full flex flex-col px-12 pt-3" action="">
-              <label className='font-semibold pb-1 pt-4' htmlFor="">Email</label>
-              <input type="text" placeholder='Email' className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black" />
-              <label className='font-semibold pb-1 pt-4' htmlFor="">Password</label>
-              <input type="text" placeholder='Password' className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black" />
-              <button className="font-semibold py-3 px-4 mt-8 rounded-md ring-1 ring-black primaryBgColor">
-                Login
-              </button>
-              <h1 className="font-semibold place-self-center pt-6"><span className='primaryColor'> <Link to={'/resetpassword'}>Forgot Password?</Link></span> </h1>
-            </form>
+  const handleSubmit = async (event) => {
+    try {
+      await axios.post(process.env.API_URL + '/login', { email, password });
+      alert('login success');
+    }
+    catch (err) {
+      console.error(err);
+    }
+    event.preventDefault();
+  }
+
+  return (
+    <div className="p-32 mainBgColor">
+      <div className="w-full h-screen flex items-start">
+        <div className="relative w-1/2 h-full flex flex-col rounded-xl primaryBgColor">
+          <div className="absolute top-[14%] left-[13%]">
+            <h1 className="text-white font-semibold text-5xl">
+              DON’T FORGET TO <br />
+              COMPLETE YOUR <br />
+              PROFILE
+            </h1>
+            <div className="flex pt-10 ">
+              <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
+                <p className="text-white font-semibold text-3xl">1</p>
+              </div>
+              <p className="pl-5 text-white font-light text-3xl">
+                Use that email address and <br />
+                valid for account verification
+              </p>
+            </div>
+            <div className="flex pt-6 ">
+              <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
+                <p className="text-white font-semibold text-3xl">2</p>
+              </div>
+              <p className="pt-3 pl-5 text-white font-light text-3xl">
+                Complete the registration form
+              </p>
+            </div>
+            <div className="flex pt-6 ">
+              <div className="bg-white rounded-xl bg-opacity-40 w-16 h-16 flex items-center justify-center relative z-10">
+                <p className="text-white font-semibold text-3xl">3</p>
+              </div>
+              <p className="pl-5 text-white font-light text-3xl">
+                Congratulations on becoming a <br />
+                DosenKu member
+              </p>
+            </div>
+            <button className="w-full text-white py-3 px-4 mt-12 rounded-md ring-1 ring-black secondaryBgColor">
+              <Link to={'/signup'}>Make new account</Link>
+            </button>
           </div>
         </div>
+
+        <div className="w-1/2 h-full flex flex-col p-20 pt-60 bg-white rounded-xl">
+          <h1 className="font-semibold text-5xl place-self-center pb-10">Login</h1>
+          <form className="w-full flex flex-col px-12 pt-3" onSubmit={handleSubmit}>
+
+            <label className='font-semibold pb-1 pt-4' htmlFor="">Email</label>
+            <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+                type="text"
+                placeholder='Email'
+                value={email}
+                onChange={(event) => setEmail(event.target.value)} />
+
+            <label className='font-semibold pb-1 pt-4' htmlFor="">Password</label>
+            <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+                type="password"
+                placeholder='Password'
+                value={password}
+                onChange={(event) => setPassword(event.target.value)} />
+
+            <button className="font-semibold py-3 px-4 mt-8 rounded-md ring-1 ring-black primaryBgColor">
+              Login
+            </button>
+
+            <h1 className="font-semibold place-self-center pt-6"><span className='primaryColor'> <Link to={'/resetpassword'}>Forgot Password?</Link></span> </h1>
+
+          </form>
+        </div>
       </div>
+    </div>
   )
 }
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,6 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Signup = () => {
+<<<<<<< Updated upstream
+=======
+  const [fullname,    setFullname]    = React.useState('');
+  const [email,       setEmail]       = React.useState('');
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [school,      setSchool]      = React.useState('');
+  const [password,    setPassword]    = React.useState('');
+
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+
+  const handleSubmit = async (event) => {
+    if (password !== confirmPassword) {
+      alert('password doesnt match');
+    }
+    else {
+      try {
+        await axios.post(process.env.API_URL + '/register', { fullname, email, phoneNumber, school, password });
+        alert('register success');
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+    event.preventDefault();
+  }
+
+>>>>>>> Stashed changes
   return (
     <div className="p-32 mainBgColor">
       <div className="w-full h-screen flex items-start">
@@ -41,6 +68,7 @@ export const Signup = () => {
           </div>
         </div>
 
+<<<<<<< Updated upstream
         <div className="w-1/2 h-full flex flex-col p-20 bg-white rounded-xl">
           <h1 className="font-semibold text-5xl place-self-center pb-10">
             Create an Account
@@ -105,6 +133,60 @@ export const Signup = () => {
             </h1>
           </form>
         </div>
+=======
+      <div className="w-1/2 h-full flex flex-col p-20 bg-white rounded-xl">
+        <h1 className="font-semibold text-5xl place-self-center pb-10">Create an Account</h1>
+        <form className="w-full flex flex-col px-12 pt-3" onSubmit={handleSubmit}>
+          <label className='font-semibold pb-1' htmlFor="">Fullname</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="text"
+              placeholder='Fullname'
+              value={fullname}
+              onChange={(event) => setFullname(event.target.value)} />
+
+          <label className='font-semibold pb-1 pt-4' htmlFor="">Email</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="text"
+              placeholder='Email'
+              value={email}
+              onChange={(event) => setEmail(event.target.value)} />
+
+          <label className='font-semibold pb-1 pt-4' htmlFor="">Phone Number</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="text"
+              placeholder='+62'
+              value={phoneNumber}
+              onChange={(event) => setPhoneNumber(event.target.value)} />
+
+          <label className='font-semibold pb-1 pt-4' htmlFor="">School/Universities</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="text"
+              placeholder='Universities...'
+              value={school}
+              onChange={(event) => setSchool(event.target.value)} />
+
+          <label className='font-semibold pb-1 pt-4' htmlFor="">Password</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="password"
+              placeholder='Password'
+              value={password}
+              onChange={(event) => setPasssword(event.target.value)} />
+
+          <label className='font-semibold pb-1 pt-4' htmlFor="">Confirm Password</label>
+          <input className="block w-full rounded-md border-0 py-3 pl-4 pr-10 ring-1 ring-black"
+              type="password"
+              placeholder='Confirm Password'
+              value={confirmPassword}
+              onChange={(event) => setConfirmPasssword(event.target.value)} />
+
+          <button className="font-semibold py-3 px-4 mt-8 rounded-md ring-1 ring-black primaryBgColor">
+            Register
+          </button>
+
+          <h1 className="font-semibold place-self-center pt-6">Already have an account? <span className="primaryColor"><Link to={'/login'}>Sign in</Link></span> </h1>
+
+        </form>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
